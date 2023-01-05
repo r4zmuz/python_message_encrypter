4ae0ffe71dbe2b7387194b5e7f5415a786c88d70
r4zmuz <rasmus.spiegel@usa.com>
2023-01-05 16:01:33 +0200

    first desktop version

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import sys
+from idlelib import window
+from turtle import window_width, window_height
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
 from cryptography.fernet import Fernet
+
 
 def generate_key():
     """Generates a key for encrypting and decrypting messages."""
@@ -12,7 +19,7 @@ def encrypt(fernet, message):
     """Encrypts the given message with the given Fernet instance."""
     # Encode the message to a byte string before encrypting it
     enc_message = fernet.encrypt(message.encode())
-    return enc_message
+    return enc_message.decode()
 
 def decrypt(fernet, enc_message):
     """Decrypts the given encrypted message with the given Fernet instance."""
@@ -22,39 +29,58 @@ def decrypt(fernet, enc_message):
     dec_message = fernet.decrypt(enc_message).decode()
     return dec_message
 
-def main():
-    """
-    Welcome message and main loop of the message encryption and decryption application.
-    To exit the application, press X anytime and hit enter.
-    """
-    print("\nWelcome to message encryption and decryption application\n"
-          "To exit application press X anytime and hit enter\n")
-    while True:
-        print("Do you want to encrypt or decrypt the message?")
-        user_input = input("Press E for encrypt or D for decrypt and hit enter: ")
-        user_input = user_input.lower()
-        if user_input == "d":
-            try:
-                enc_message = input("\nEnter message to decrypt and hit enter: ")
-                dec_message = decrypt(fernet, enc_message)
-                print("\ndecrypted string: ", dec_message)
-                print("")
-            except Exception as e:
-                print("\nAn error occurred:\n", e)
-        elif user_input == "e":
-            message = input("\nEnter message to encrypt and hit enter: ")
-            enc_message = encrypt(fernet, message)
-            print("\noriginal string: ", message)
-            print("encrypted message: ", enc_message)
-            print("")
-        elif user_input == "x":
-            exit()
-        else:
-            print("\nEnter valid selection\n")
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    def initUI(self):
+
+        # Create the widgets
+        self.setGeometry(100, 100, 600, 100) #set window size
+        self.label = QLabel("Enter message:", self)
+        self.line_edit = QLineEdit(self)
+        self.encrypt_button = QPushButton("Encrypt", self)
+        self.decrypt_button = QPushButton("Decrypt", self)
+        self.enc_label = QLabel("", self)  # create an instance of QLabel
+
+        # Set up the layout
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.label)
+        layout.addWidget(self.line_edit)
+        layout.addWidget(self.enc_label)
+        layout.addWidget(self.encrypt_button)
+        layout.addWidget(self.decrypt_button)
+        self.setLayout(layout)
+
+        # Connect the buttons to the slots
+        self.encrypt_button.clicked.connect(self.encrypt)
+        self.decrypt_button.clicked.connect(self.decrypt)
+
+    def encrypt(self):
+        message = self.line_edit.text()
+        enc_message = encrypt(fernet, message)
+        self.label.setText("Encrypted message:")
+        self.enc_label.setText(enc_message)
+
+
+    def decrypt(self):
+        enc_message = self.line_edit.text()
+        dec_message = decrypt(fernet, enc_message)
+        self.label.setText("Decrypted message:")
+        self.enc_label.setText(dec_message)
 
 if __name__ == '__main__':
     # Generate a key for encryption and decryption
     key = generate_key()
-    # Create a Fernet instance with the key
+    # # Create a Fernet instance with the key
     fernet = create_fernet(key)
-    main()
+    # Create an instance of QApplication
+    app = QApplication(sys.argv)
+    # Create an instance of MainWindow
+    window = MainWindow()
+    #set window size
+    # Show the window
+    window.show()
+    # Run the application's event loop
+    sys.exit(app.exec_())
+
